004c3126f34681e3734b39bcfdbefb04de0ba27c
Xianhuii <xianhuii@foxmail.com>
2022-10-13 20:28:18 +0800

    vault backup: 2022-10-13 20:28:17

--- Spring/CORS/demo/servlet-mvc/src/main/java/com/example/servletmvc/controller/CorsController.java
+++ Spring/CORS/demo/servlet-mvc/src/main/java/com/example/servletmvc/controller/CorsController.java
@@ -3,6 +3,9 @@ package com.example.servletmvc.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * 测试CORS的Controller
  *
@@ -13,6 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CorsController {
     @RequestMapping("cors")
     public String cors() {
+        System.out.println("Hello Cors!");
+        return "Hello Cors!";
+    }
+    @RequestMapping("corsResponse")
+    public String corsResponse(HttpServletResponse response) {
+        response.addHeader("Access-Control-Allow-Origin", "*");
         System.out.println("Hello Cors!");
         return "Hello Cors!";
     }
