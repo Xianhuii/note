bb2d2e67d97a92fa78d456cfc686385268d614c9
Xianhuii <xianhuii@foxmail.com>
2022-10-28 10:22:44 +0800

    vault backup: 2022-10-28 10:22:43

---
+++ 前端/front-end-demo/src/main/java/com/example/frontenddemo/FrontEndDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.frontenddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontEndDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontEndDemoApplication.class, args);
+    }
+
+}
---
+++ 前端/front-end-demo/src/test/java/com/example/frontenddemo/FrontEndDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frontenddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontEndDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
