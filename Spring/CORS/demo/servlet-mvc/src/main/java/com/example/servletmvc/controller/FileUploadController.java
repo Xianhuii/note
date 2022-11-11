6e78e30cdad46ea829537754681fcfd8b529c368
Xianhuii <xianhuii@foxmail.com>
2022-11-11 14:41:54 +0800

    vault backup: 2022-11-11 14:41:53

---
+++ Spring/CORS/demo/servlet-mvc/src/main/java/com/example/servletmvc/controller/FileUploadController.java
@@ -0,0 +1,23 @@
+package com.example.servletmvc.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+/**
+ * 测试文件上传的Controller
+ *
+ * @author jxh
+ * @date 2022年10月13日 12:55
+ */
+@RestController
+@RequestMapping("file")
+public class FileUploadController {
+
+    @PostMapping("/upload")
+    public String upload(MultipartHttpServletRequest request) {
+        System.out.println(request);
+        return "Hello Cors!";
+    }
+}
