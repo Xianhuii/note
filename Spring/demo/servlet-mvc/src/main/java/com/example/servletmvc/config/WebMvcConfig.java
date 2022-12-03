05fd349ade2442d112d86f6df096e077bdb9302f
xianhuii <xianhuii@foxmail.com>
2022-12-03 12:19:36 +0800

    vault backup: 2022-12-03 12:19:36

--- Spring/demo/servlet-mvc/src/main/java/com/example/servletmvc/config/WebMvcConfig.java
+++ Spring/demo/servlet-mvc/src/main/java/com/example/servletmvc/config/WebMvcConfig.java
@@ -36,7 +36,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-
+        // 添加自定义拦截器
     }
 
     @Override
