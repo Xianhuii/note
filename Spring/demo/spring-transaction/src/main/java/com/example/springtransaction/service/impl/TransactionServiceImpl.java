fbd550acd03dea9f283972b47971d7c49e67e5f5
xianhuii <xianhuii@foxmail.com>
2023-01-03 23:19:56 +0800

    vault backup: 2023-01-03 23:19:56

--- Spring/demo/spring-transaction/src/main/java/com/example/springtransaction/service/impl/TransactionServiceImpl.java
+++ Spring/demo/spring-transaction/src/main/java/com/example/springtransaction/service/impl/TransactionServiceImpl.java
@@ -1,11 +1,15 @@
 package com.example.springtransaction.service.impl;
 
 import com.example.springtransaction.service.ITransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TransactionServiceImpl implements ITransactionService {
+    @Autowired
+    private PlatformTransactionManager platformTransactionManager;
 
     @Transactional
     @Override
