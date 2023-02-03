cbd7d398c9c41637be741ee7d54505ce315ce9d4
xianhuii <xianhuii@foxmail.com>
2023-02-03 15:21:17 +0800

    vault backup: 2023-02-03 15:21:16

--- Spring/demo/spring-core/src/main/java/annotation/configuration/Main.java
+++ Spring/demo/spring-core/src/main/java/annotation/configuration/Main.java
@@ -1,2 +1,31 @@
-package annotation.configuration;public class Main {
+package annotation.configuration;
+
+import annotation.configuration.config.AppConfig;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+//        beanFactory();
+        applicationContext();
+    }
+
+    public static void beanFactory() {
+        BeanFactory beanFactory = new DefaultListableBeanFactory();
+        BeanDefinitionRegistry beanDefinitionRegistry = (BeanDefinitionRegistry) beanFactory;
+        AnnotatedBeanDefinitionReader reader = new AnnotatedBeanDefinitionReader(beanDefinitionRegistry);
+        reader.registerBean(AppConfig.class);
+        Object object = beanFactory.getBean("object");
+        System.out.println(object);
+    }
+
+    public static void applicationContext() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Object object = context.getBean("object");
+        System.out.println(object);
+    }
 }
--- Spring/demo/spring-core/src/main/java/annotation/configuration/config/AppConfig.java
+++ Spring/demo/spring-core/src/main/java/annotation/configuration/config/AppConfig.java
@@ -1,2 +1,13 @@
-package annotation.configuration.config;public class AppConfig {
+package annotation.configuration.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Object object() {
+        return new Object();
+    }
 }
