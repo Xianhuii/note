961d7a0ec7a37cd224dcf7ba359ef74539678d49
xianhuii <xianhuii@foxmail.com>
2023-02-16 21:58:23 +0800

    vault backup: 2023-02-16 21:58:23

---
+++ Java/java-code/src/main/java/thread/CallableDemo.java
@@ -0,0 +1,26 @@
+package thread;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class CallableDemo {
+    public static void main(String[] args) {
+        Callable<Integer> task = new Callable() {
+            @Override
+            public Integer call() throws Exception {
+                return 100;
+            }
+        };
+        FutureTask futureTask = new FutureTask(task);
+        Thread thread = new Thread(futureTask);
+        thread.start();
+        try {
+            System.out.println(futureTask.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
