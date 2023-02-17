b47afa54612435c9e37944836657db4e821e67c3
xianhuii <xianhuii@foxmail.com>
2023-02-17 14:31:12 +0800

    vault backup: 2023-02-17 14:31:12

---
+++ Java/java-code/src/main/java/thread/ThreadPoolExecutorDemo.java
@@ -0,0 +1,33 @@
+package thread;
+
+import java.util.concurrent.*;
+
+public class ThreadPoolExecutorDemo {
+    public static void main(String[] args) {
+        BlockingQueue<Runnable> blockingQueue = new ArrayBlockingQueue<>(3);
+        ThreadFactory threadFactory = Executors.defaultThreadFactory();
+        RejectedExecutionHandler rejectedExecutionHandler = new ThreadPoolExecutor.CallerRunsPolicy();
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                3,
+                6,
+                1,
+                TimeUnit.SECONDS,
+                blockingQueue,
+                threadFactory,
+                rejectedExecutionHandler);
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + ": run()");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        for (int i = 0; i < 10; i++) {
+            threadPoolExecutor.submit(task);
+        }
+    }
+}
