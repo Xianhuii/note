c59e56da7a2b6d5f771251838ad5041defe10c5b
xianhuii <xianhuii@foxmail.com>
2023-02-20 12:11:02 +0800

    vault backup: 2023-02-20 12:11:02

---
+++ Java/java-code/src/main/java/thread/ScheduledThreadPoolExecutorDemo.java
@@ -0,0 +1,32 @@
+package thread;
+
+import java.util.concurrent.*;
+
+public class ScheduledThreadPoolExecutorDemo {
+    public static void main(String[] args) {
+        ThreadFactory threadFactory = Executors.defaultThreadFactory();
+        RejectedExecutionHandler rejectedExecutionHandler = new ThreadPoolExecutor.CallerRunsPolicy();
+        // 1、创建线程池
+        ScheduledThreadPoolExecutor scheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(
+                3,
+                threadFactory,
+                rejectedExecutionHandler);
+        // 2、创建任务
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + ": run()");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        // 执行延时任务
+//        scheduledThreadPoolExecutor.schedule(task, 1, TimeUnit.SECONDS);
+        // 执行定时任务
+        scheduledThreadPoolExecutor.scheduleAtFixedRate(task, 0, 1, TimeUnit.SECONDS);
+//        scheduledThreadPoolExecutor.shutdown();
+    }
+}
