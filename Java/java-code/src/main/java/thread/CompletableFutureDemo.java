d533c795e2e6edbf0d60728d0625a43ecd39432d
xianhuii <xianhuii@foxmail.com>
2023-02-28 11:47:31 +0800

    vault backup: 2023-02-28 11:47:30

--- Java/java-code/src/main/java/thread/CompletableFutureDemo.java
+++ Java/java-code/src/main/java/thread/CompletableFutureDemo.java
@@ -1,2 +1,18 @@
-package thread;public class CompletableFutureDemo {
+package thread;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.FutureTask;
+
+public class CompletableFutureDemo {
+    public static void main(String[] args) {
+        Callable<String> task = new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return null;
+            }
+        };
+        CompletableFuture<String> completableFuture = new CompletableFuture<>();
+        
+    }
 }
