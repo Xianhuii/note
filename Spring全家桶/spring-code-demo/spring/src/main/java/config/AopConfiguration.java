b3e6dc0d3a0565cbfa693ef23c8d19e749bb1de3
xianhuii <xianhuii@foxmail.com>
2023-10-07 11:23:16 +0800

    vault backup: 2023-10-07 11:23:16

--- Spring全家桶/spring-code-demo/spring/src/main/java/config/AopConfiguration.java
+++
@@ -1,9 +0,0 @@
-package config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-@EnableAspectJAutoProxy
-@Configuration
-public class AopConfiguration {
-}
--- Spring全家桶/spring-code-demo/springboot/src/main/java/com/xianhuii/springboot/service/TransactionService.java
+++
@@ -1,4 +0,0 @@
-package com.xianhuii.springboot.service;
-
-public interface TransactionService {
-}
